97c0d8704d138c74a0ab965ad5ba95efbbfc942f
vincenek <kenechukwu130@gmail.com>
2025-08-12 21:54:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-72x72.png',
   '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
   '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
   '/icons/icon-512x512.png'
 ];
 
